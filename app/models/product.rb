d27b2c374b20e554656880d6a012df1b15d1f8b5
Cyrille Varin <cyrille.varin@live.fr>
2023-07-18 18:46:56 +0200

    seed debug

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   has_many_attached :images
   has_many :likes, dependent: :destroy
   has_many :offers
-  belongs_to :user
 
   validates :marque, :modele, :content, presence: true
 end
