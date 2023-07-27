58911e3e983d4eeb2ae63b9c967c3d4fbfb9886e
Cyrille Varin <cyrille.varin@live.fr>
2023-07-27 18:57:44 +0200

    details card et show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @sorted_offers = sort_offers
   end
 
   def liked
@@ -38,5 +39,20 @@ class ProductsController < ApplicationController
         @products = @products.order(marque: :asc)
       end
     end
+  end
+
+  def sort_offers
+    sorted_offers = @product.offers
+    if params[:sort].present?
+      case params[:sort]
+      when "price_asc"
+        sorted_offers = sorted_offers.order(price: :asc)
+      when "price_desc"
+        sorted_offers = sorted_offers.order(price: :desc)
+      when "alphabetical"
+        sorted_offers = sorted_offers.order(etat: :asc)
+      end
+    end
+    sorted_offers
   end
 end
